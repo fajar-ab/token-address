eec53dd02fe191a02794a79a64c8fbfed2c491f3
fajar-ab <fajarfanz378@gmail.com>
2023-08-26 15:18:19 +0700

    mine xmr

---
+++ xmrig-6.20.0/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Menjalankan perintah xmrig
+./xmrig --donate-level 1 -o sg.monero.herominers.com:1111 -u 86AgQuzDWnYLGgVjfSZCEeQc6WgamaZ46R9oKW1hetwGiuChRMv3xyXdojD2JoRBhJe57iiR97ZLCiqgNeePMnQh77YemJA -p MINE -a rx/0 -k 
+
+# Menambahkan perintah pause untuk memberikan waktu sejenak sebelum script berakhir
+read -p "Press Enter to exit..."
